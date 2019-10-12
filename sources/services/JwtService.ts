95061dce285317fc5a7a21bda36b5c4c38cce5f8
Guillaume Benard <guillaume@benard.io>
2019-10-12 13:59:20 +0200

    Create Service Jwt

---
+++ sources/services/JwtService.ts
@@ -0,0 +1,18 @@
+import Jwt from "jsonwebtoken";
+
+export default class JwtService {
+  private readonly privateKey: string;
+
+  constructor(privateKey: string) {
+    this.privateKey = privateKey;
+  }
+
+  public readonly tokenize = (data: object): string => {
+    return Jwt.sign({ data }, this.privateKey);
+  };
+
+  public readonly decode = (token: string): object => {
+    const { data } = Jwt.verify(token, this.privateKey) as { data: object };
+    return data;
+  };
+}
