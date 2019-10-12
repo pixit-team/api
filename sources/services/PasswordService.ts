09ef2d17b3d35caee5de33980c057b9affecccbf
Guillaume Benard <guillaume@benard.io>
2019-10-12 13:55:16 +0200

    Create PasswordService

---
+++ sources/services/PasswordService.ts
@@ -0,0 +1,36 @@
+import Bcrypt from "bcrypt";
+
+/**
+ * Simple utility allowing to cipher and match passwords
+ */
+export default class PasswordService {
+  private static readonly NB_ROUNDS: number = 10;
+
+  /**
+   * Cipher the given password
+   *
+   * @param password  The password to cipher
+   *
+   * @return The ciphered password
+   */
+  public readonly cipherPassword = async (
+    password: string,
+  ): Promise<string> => {
+    return Bcrypt.hash(password, PasswordService.NB_ROUNDS);
+  };
+
+  /**
+   * Check if the given password corresponds to the ciphered one
+   *
+   * @param cipheredPassword  The ciphered hash to match against
+   * @param password  The password to validate
+   *
+   * @return `true` if the password is compatible with this hash, `false` otherwise
+   */
+  public readonly validatePassword = async (
+    cipheredPassword: string,
+    password: string,
+  ): Promise<boolean> => {
+    return Bcrypt.compare(password, cipheredPassword);
+  };
+}
